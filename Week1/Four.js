0cecc363ce3529c59db7b5e5d2e3a37a671f6717
InfuRides <tech.infu@infurides.com>
2025-06-21 13:28:13 +0530

    4

---
+++ Week1/Four.js
@@ -0,0 +1,10 @@
+function outerFunction() {
+  const message = "Hello from the closure!";
+
+  return function innerFunction() {
+    console.log(message);
+  };
+}
+
+const storedFunction = outerFunction();
+storedFunction(); 
